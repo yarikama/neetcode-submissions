8572b2adba6f38b51d6e7d9436de4007fc0c1fe4
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-14 17:02:55 -0500

    Add: merge-k-sorted-linked-lists - submission-3

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-3.py
@@ -0,0 +1,46 @@
+from heapq import heapify, heappop, heappush
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return None
+
+        interval = 1
+        while interval < len(lists):
+            for i in range(0, len(lists) - interval, interval * 2):
+                lists[i] = self.merge(lists[i], lists[i + interval])
+            interval *= 2
+
+        return lists[0]
+
+    
+    def merge(
+        self, list1: Optional[ListNode], list2: Optional[ListNode]
+    ) -> Optional[ListNode]:
+        # if not list1:
+        #     return list2
+        
+        # if not list2:
+        #     return list1
+
+        head = curr = ListNode()
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+
+        curr.next = list1 or list2 # important if there is no more connections
+
+        return head.next
+        
