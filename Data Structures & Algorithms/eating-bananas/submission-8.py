dceca2c84869769f9081ed7a7e7b385005ff2299
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-17 01:10:39 -0500

    Add: eating-bananas - submission-8

---
+++ Data Structures & Algorithms/eating-bananas/submission-8.py
@@ -0,0 +1,26 @@
+from math import ceil
+
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        min_rate, max_rate = 1, max(piles)
+
+        if h == len(piles): return max_rate
+
+        def canFinish(rate: int) -> bool:
+            hrs = 0
+            for pile in piles:
+                hrs += ceil(pile / rate)
+                if hrs > h: return False
+            return True
+
+        ans = max_rate
+        while min_rate <= max_rate:
+            rate = (min_rate + max_rate) // 2
+
+            if canFinish(rate):
+                ans = rate
+                max_rate = rate - 1
+            else:
+                min_rate = rate + 1
+
+        return ans
