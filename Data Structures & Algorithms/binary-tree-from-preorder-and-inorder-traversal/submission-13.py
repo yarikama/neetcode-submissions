04ba4091072aff876e8ca5e911bce210b6af3759
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-18 16:55:09 -0500

    Add: binary-tree-from-preorder-and-inorder-traversal - submission-13

---
+++ Data Structures & Algorithms/binary-tree-from-preorder-and-inorder-traversal/submission-13.py
@@ -0,0 +1,59 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        
+        hashmap = {}
+
+        for idx, val in enumerate(inorder):
+            hashmap[val] = idx
+
+        def helper(s_pre, e_pre, s_in, e_in):
+            if s_pre > e_pre:
+                return None
+
+            node = TreeNode(preorder[s_pre])
+
+            idx = hashmap[preorder[s_pre]]
+            left_size = idx - s_in
+
+            node.left = helper(
+                s_pre + 1, s_pre + left_size,
+                s_in, s_in + left_size - 1
+            )
+            node.right = helper(
+                s_pre + 1 + left_size, e_pre,
+                idx + 1, e_in
+            )
+
+            return node
+
+        return helper(0, len(preorder)-1, 0, len(inorder)-1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+        # if not preorder: return None
+
+        # node = TreeNode(preorder[0])
+
+        # for idx, val in enumerate(inorder):
+        #     if val == preorder[0]:
+        #         node.left = self.buildTree(preorder[1:1+idx], inorder[:idx])
+        #         node.right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+
+        # return node
