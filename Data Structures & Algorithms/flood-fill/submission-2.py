73271fbd2e8ae547057f8ea1f5e07e3d85e0568d
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-20 12:45:17 -0500

    Add: flood-fill - submission-2

---
+++ Data Structures & Algorithms/flood-fill/submission-2.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        queue = deque([[sr, sc]])
+        dirs = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+        origin = image[sr][sc]
+        if origin == color:
+            return image
+        
+        while queue:
+            for i in range(len(queue)):
+                r, c = queue.popleft()
+                if min(r, c) < 0 or r > len(image)-1 or c > len(image[0])-1 or image[r][c] != origin:
+                    continue
+                
+                image[r][c] = color
+                for dr, dc in dirs:
+                    queue.append([r+dr, c+dc])
+
+        return image
