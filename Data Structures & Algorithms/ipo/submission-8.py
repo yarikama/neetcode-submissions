c1f4dc153341b7492cd56a47a456a930e7dc8a10
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-22 00:33:33 -0500

    Add: ipo - submission-8

---
+++ Data Structures & Algorithms/ipo/submission-8.py
@@ -0,0 +1,28 @@
+from heapq import heappush, heappop, heappush_max, heapify, heapify_max, heappop_max
+
+class Solution:
+    def findMaximizedCapital(
+        self, k: int, w: int, profits: List[int], capital: List[int]
+    ) -> int:
+        cap_and_pros = [ (cap, pro) for cap, pro in zip(capital, profits) ]
+        cap_and_pros.sort()
+
+        n = len(profits)
+        h = []
+
+        j = 0
+        for i in range(k):
+            while j < n and w >= cap_and_pros[j][0]:
+                heappush_max(h, cap_and_pros[j][1])
+                j += 1
+            
+            if not h:
+                break
+
+            w += heappop_max(h)
+            
+        return w
+            
+
+
+        
