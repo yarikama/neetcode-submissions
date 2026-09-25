05548f6aae1552d88189ceb368ea325848570547
Yarikama <125861728+yarikama@users.noreply.github.com>
2026-09-25 15:47:09 -0500

    Add: count-subsequences - submission-5

---
+++ Data Structures & Algorithms/count-subsequences/submission-5.py
@@ -0,0 +1,36 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:        
+        n, m = len(s), len(t)
+
+        if m > n:
+            return 0
+
+        cache = [1] + [0] * m
+
+        for r in range(1, n+1):
+            new_cache = [0] * (m + 1)
+            new_cache[0] = 1
+            for c in range(1, m+1):
+                if s[r-1] == t[c-1]:
+                    new_cache[c] = cache[c-1] + cache[c]
+                else:
+                    new_cache[c] = cache[c]
+
+            cache = new_cache
+        
+        return cache[-1] 
+
+    # def numDistinct(self, s: str, t: str) -> int:  
+        # n, m = len(s), len(t)
+
+        # if m > n:
+        #     return 0
+
+        # cache = [1] + [0] * m
+
+        # for r in range(1, n+1):
+        #     for c in range(m, 0, -1):
+        #         if s[r-1] == t[c-1]:
+        #             cache[c] += cache[c-1]
+        
+        # return cache[-1] 
